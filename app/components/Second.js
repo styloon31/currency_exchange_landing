368888d178c3239f218c59bf47bfb4a96229a8c2
Divyansh Rana <99189937+styloon31@users.noreply.github.com>
2025-01-13 15:09:24 +0530

    forex

--- app/components/Second.js
+++ app/components/Second.js
@@ -12,14 +12,14 @@ export default function Second() {
           {/* Right Section */}
           <div className="flex items-center justify-center">
             <p className="font-Helvetica text-base md:text-xl text-justify">
-              7janpath provides essential services that enable individuals and
+              7janpath forex provides essential services that enable individuals and
               businesses to convert one currency into another. The company plays a
               critical role in facilitating international trade, travel, and
               investments by offering competitive exchange rates for buying and
               selling foreign currencies. It caters to travelers needing foreign
               cash, businesses conducting transactions in different currencies,
               and investors managing foreign assets. In addition to exchanging
-              physical cash, 7janpath also provides services like wire transfers,
+              physical cash, 7janpath forex also provides services like wire transfers,
               forex cards, remittances, and online trading platforms for currency
               exchange. Its expertise in monitoring global exchange rate
               fluctuations ensures customers receive accurate and reliable
