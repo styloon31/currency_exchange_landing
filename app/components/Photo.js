3810565f820b7e317c95ae63bb5cc6bea30bef87
shreyansh <shreyansh@bitstobug.com>
2025-01-23 00:16:34 +0530

    image

--- app/components/Photo.js
+++ app/components/Photo.js
@@ -1,6 +1,6 @@
 export default function Photo(){
     return(
-        <section className="w-screen h-[30vh] md:h-dvh overflow-x-hidden relative bg-[url('/Images/bg.jpeg')] bg-no-repeat bg-center bg-contain">
+        <section className="w-screen h-[30vh] md:h-dvh overflow-x-hidden relative bg-[url('/Images/bg.png')] bg-no-repeat bg-center bg-contain">
 
         </section>
     )
